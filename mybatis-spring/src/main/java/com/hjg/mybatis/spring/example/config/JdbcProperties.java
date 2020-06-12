eb503d2f284b1e7f1f842b57d6241cd2aae7a59f
hjg <computer_cka@163.com>
2020-06-12 16:20:08 +0800

    测试@Value默认值。

--- mybatis-spring/src/main/java/com/hjg/mybatis/spring/example/config/JdbcProperties.java
+++ mybatis-spring/src/main/java/com/hjg/mybatis/spring/example/config/JdbcProperties.java
@@ -14,4 +14,10 @@ public class JdbcProperties {
 
     @Value("${db.url}")
     private String url;
+
+    /**
+     * 使用@Value设置默认值，与xml配置时用法类似。
+     */
+    @Value("${db.test:AAAAA}")
+    private String test;
 }
