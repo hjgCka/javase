d0c723163e5ef4437f0b6d6a86e34d3daafdfe07
hjg <computer_cka@163.com>
2020-09-24 10:06:13 +0800

    更新ftp模块测试用例。

---
+++ ftp/src/test/java/com/hug/ftp/FtpUtilTest.java
@@ -0,0 +1,56 @@
+package com.hug.ftp;
+
+import com.hjg.ftp.FtpUtil;
+import org.apache.commons.net.ftp.FTPClient;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @description:
+ * @author: hjg
+ * @createdOn: 2020/9/24
+ */
+public class FtpUtilTest {
+
+    private static final String address = "10.168.55.88";
+    private static final int port = 21;
+
+    private static final String username = "ftpuser";
+    private static final String password = "acct4rcs";
+
+    @Test
+    public void listFileTest() {
+
+        String remote = "/";
+
+        FTPClient ftpClient = FtpUtil.connectFtpServer(address, port);
+        boolean successed = FtpUtil.loginFtpServer(ftpClient, username, password);
+        if(successed) {
+            List<String> list = FtpUtil.listFtpFiles(ftpClient, remote);
+            System.out.println("list = " + list);
+
+            FtpUtil.logoutFtpServer(ftpClient);
+        } else {
+            System.out.println("登陆失败");
+        }
+    }
+
+    @Test
+    public void downloadFileTest() {
+
+        String remote = "/data.xlsx", local = "e://data.xlsx";
+
+        FTPClient ftpClient = FtpUtil.connectFtpServer(address, port);
+        boolean successed = FtpUtil.loginFtpServer(ftpClient, username, password);
+        if(successed) {
+
+            int count = FtpUtil.downloadFtpFile(ftpClient, remote, local);
+            if(count == 1) {
+                System.out.println("下载次数 count = " + count);
+            }
+        } else {
+            System.out.println("登陆失败");
+        }
+    }
+}
