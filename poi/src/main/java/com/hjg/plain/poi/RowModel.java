7b80936b6e35b24adb436adc0cc197031e88b0f0
hjg <computer_cka@163.com>
2020-09-28 16:45:26 +0800

    新增cell格式解析。

---
+++ poi/src/main/java/com/hjg/plain/poi/RowModel.java
@@ -0,0 +1,24 @@
+package com.hjg.plain.poi;
+
+import lombok.Data;
+
+/**
+ * @description:
+ * @author: hjg
+ * @createdOn: 2020/9/28
+ */
+@Data
+public class RowModel {
+
+    private String id;
+
+    private String userId;
+
+    private String acceptDate;
+
+    private String startDate;
+
+    private String endDate;
+
+    private String phone;
+}
--- poi/src/main/java/com/hjg/plain/poi/TextExtractionApp.java
+++ poi/src/main/java/com/hjg/plain/poi/TextExtractionApp.java
@@ -2,16 +2,21 @@ package com.hjg.plain.poi;
 
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.util.XMLHelper;
 import org.apache.poi.xssf.eventusermodel.XSSFReader;
 import org.apache.poi.xssf.model.SharedStringsTable;
+import org.apache.poi.xssf.model.StylesTable;
 import org.xml.sax.*;
 import org.xml.sax.helpers.DefaultHandler;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -26,7 +31,8 @@ public class TextExtractionApp {
         XSSFReader xssfReader = new XSSFReader( pkg );
 
         SharedStringsTable sst = xssfReader.getSharedStringsTable();
-        XMLReader parser = fetchSheetParser(sst);
+        StylesTable stylesTable = xssfReader.getStylesTable();
+        XMLReader parser = fetchSheetParser(sst, stylesTable);
 
         InputStream sheet = xssfReader.getSheet("rId1");
         InputSource sheetSource = new InputSource(sheet);
@@ -35,9 +41,10 @@ public class TextExtractionApp {
         sheet.close();
     }
 
-    public XMLReader fetchSheetParser(SharedStringsTable sst) throws SAXException, ParserConfigurationException {
+    public XMLReader fetchSheetParser(SharedStringsTable sst, StylesTable stylesTable) throws SAXException,
+            ParserConfigurationException {
         XMLReader parser = XMLHelper.newXMLReader();
-        ContentHandler handler = new TextExtractionApp.SheetHandler(sst);
+        ContentHandler handler = new TextExtractionApp.SheetHandler(sst, stylesTable);
         parser.setContentHandler(handler);
         return parser;
     }
@@ -45,31 +52,57 @@ public class TextExtractionApp {
     private static class SheetHandler extends DefaultHandler {
 
         private SharedStringsTable sst;
+        private StylesTable stylesTable;
         //标签的内容
         private String cellValue;
 
-        private List<String> phoneList = new ArrayList<>();
-        private boolean nextIsPhone = false;
+        private List<RowModel> phoneList = new ArrayList<>();
+        private RowModel tempRowModel;
+        //1-userid , 2-phone, 3-accept_date, 4-start_date, 5-end_date
+        private int fieldIndex = 0;
+
+        private String cellStyleStr;
+        private DataFormatter formatter = new DataFormatter();
+        private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
         //从0开始，每次+1
+        private int lineNumber = 0;
         private int batchId = 0;
 
-        private SheetHandler(SharedStringsTable sst) {
+        private SheetHandler(SharedStringsTable sst, StylesTable stylesTable) {
             this.sst = sst;
+            this.stylesTable = stylesTable;
         }
 
         @Override
         public void startElement(String uri, String localName, String name,
                                  Attributes attributes) throws SAXException {
 
+            if(name.equals("row")) {
+                tempRowModel = new RowModel();
+                lineNumber++;
+            }
+
             // c => cell
             if(name.equals("c")) {
+
                 String referenceId = attributes.getValue("r");
-                if(referenceId.startsWith("B")) {
+                if(referenceId.startsWith("A")) {
+                    fieldIndex = 1;
+                } else if(referenceId.startsWith("B")) {
                     //只获取第二列的数据，这一列是手机号
-                    nextIsPhone = true;
+                    fieldIndex = 2;
+                } else if(referenceId.startsWith("C")) {
+                    fieldIndex = 3;
+                    cellStyleStr = attributes.getValue("s");
+                } else if(referenceId.startsWith("D")) {
+                    fieldIndex = 4;
+                    cellStyleStr = attributes.getValue("s");
+                } else if(referenceId.startsWith("E")) {
+                    fieldIndex = 5;
+                    cellStyleStr = attributes.getValue("s");
                 } else {
-                    nextIsPhone = false;
+                    fieldIndex = 0;
                 }
             }
         }
@@ -77,9 +110,45 @@ public class TextExtractionApp {
         @Override
         public void endElement(String uri, String localName, String name)
                 throws SAXException {
-            if(nextIsPhone) {
-                phoneList.add(cellValue);
-                nextIsPhone = false;
+
+            if(name.equals("c")) {
+                int idx;
+                switch (fieldIndex) {
+                    case 0:
+                        break;
+                    case 1:
+                        idx = Integer.parseInt(cellValue);
+                        tempRowModel.setUserId(sst.getItemAt(idx).getString());
+                        break;
+                    case 2:
+                        tempRowModel.setPhone(cellValue);
+                        break;
+                    case 3:
+                        if(lineNumber > 1) {
+                            //表头不进行设置
+                            tempRowModel.setAcceptDate(this.getDateCellContent());
+                        }
+                        break;
+                    case 4:
+                        if(lineNumber > 1) {
+                            tempRowModel.setStartDate(this.getDateCellContent());
+                        }
+                        break;
+                    case 5:
+                        if(lineNumber > 1) {
+                            tempRowModel.setEndDate(this.getDateCellContent());
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if(name.equals("row")) {
+                if(lineNumber > 1) {
+                    //跳过表头
+                    phoneList.add(tempRowModel);
+                }
             }
 
             if(phoneList.size() == 1000) {
@@ -103,6 +172,21 @@ public class TextExtractionApp {
                 System.out.println("获得剩余手机号，进行最后一批处理。batchId = " + batchId + ", last = " + phoneList.get(length - 1));
                 phoneList.clear();
             }
+        }
+
+        private String getDateCellContent() {
+            Date date = DateUtil.getJavaDate(Double.parseDouble(cellValue));
+            String str = sdf.format(date);
+
+            /*int styleIndex = Integer.parseInt(this.cellStyleStr);
+            XSSFCellStyle style = stylesTable.getStyleAt(styleIndex);
+
+            int formatIndex = style.getDataFormat();
+            String dateFormat = style.getDataFormatString();
+
+            String str = formatter.formatRawCellContents(Double.parseDouble(cellValue), formatIndex, dateFormat);*/
+
+            return str;
         }
     }
 
