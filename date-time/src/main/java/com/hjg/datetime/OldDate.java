8348e085c5ed03a8f2dad1d133cc27ab53363756
hjg <computer_cka@163.com>
2020-11-01 16:47:11 +0800

    添加类。

---
+++ date-time/src/main/java/com/hjg/datetime/OldDate.java
@@ -0,0 +1,16 @@
+package com.hjg.datetime;
+
+import java.util.Date;
+
+/**
+ * @description:
+ * @author: hjg
+ * @createdOn: 2020/11/1
+ */
+public class OldDate {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date.getTime());
+    }
+}
